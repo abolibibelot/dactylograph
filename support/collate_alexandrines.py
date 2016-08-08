15c5110853f5ed0d964d035a02370b3fcb11d48f
Yann Schwartz <y.schwartz@criteo.com>
2016-08-08 17:09:25 +0200

    Dieresis is on board!
    In the sample verses at least

--- support/collate_alexandrines.py
+++ support/collate_alexandrines.py
@@ -6,7 +6,7 @@ ALL = "all_verses.txt"
 
 
 def extract_info(line):
-    raw_split = line.strip().split('\t')
+    raw_split = line.strip().replace('\xa0', ' ').split('\t')
     if len(raw_split) != 2:
         return None
     l = [raw_split[0]]
@@ -18,16 +18,35 @@ def has_twelve(info):
     return len(info[1].split('/')) == 12
 
 
+def add_dieresis(info):
+    splits = info[1]
+    desc = info[4]
+    candidate = desc[desc.find('sur : ') + 6:].lower()
+    phonems = ['ie', 'ia', 'ui', 'ue', 'iu']
+    orig = splits.find(candidate)
+    idx = splits.find(candidate)
+    if idx == -1:
+        return None
+    repl = None
+    for phonem in phonems:
+        if phonem in candidate:
+            repl = candidate.replace(phonem, phonem[0] + '/' + phonem[1])
+            break
+    if repl is None:
+        return None
+    info[1] = splits.replace(candidate, repl)
 
 def filter_likely(info):
     length = info[2]
-    if length == '12':
+    if 'à' not in length:
+        return length == '12'
+    if length == '12 à 13' or length == '12 à 14':
+        return True #No dieresis
+    if length == '11 à 12': #dieresis
+        add_dieresis(info)
         return True
-    if length in ['11', '12', '13'] and has_twelve(info):
-        return True
-    interv = length.strip().split(' à ')
-    if len(interv) == 2 and (int(interv[0]) <= 12 <= int(interv[1])):
-        return has_twelve(info)
+
+
     return False
 
 def merge(info):
@@ -56,9 +75,10 @@ unlikely = []
 
 for l in open(path.join(DATA_PATH, ALL), 'r', encoding='utf8'):
     verse_info = extract_info(l)
-    merge(verse_info)
+    #merge(verse_info)
     full_info.append(verse_info)
     if filter_likely(verse_info):
         likely.append(verse_info)
     else:
         unlikely.append(verse_info)
+
